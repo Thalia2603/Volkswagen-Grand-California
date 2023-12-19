7a801c83cac8c180e86d4436addca6d82aaccb2b
Thalia <thalia.soler.7e7@itb.cat>
2023-12-19 10:37:31 +0100

    Añadidos los colores del "menu"

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,28 +4,29 @@ fun main(){
     //missatge d'error que printar dins dels metodes
     val messageError="Valor incorrecte, torni a intentar"
 
-    println("  __        ___\n" +
+    println(
+        RED_BOLD+"  __        ___\n" +
             " / o\\      /o o\\\n" +
             "|   <      |   |\n" +
-            " \\__/      |,,,|")
+            " \\__/      |,,,|"+RESET)
 
     //pregunta a l'usuari si te la Volkswagen Grand California normal o la Camper Full Equip
-    println("Sel·leccioni una opció:\n1-Volkswagen Grand California\n2-Volkswagen Grand California Camper Full Equip")
+    println(CYAN_BOLD+"Sel·leccioni una opció:\n1-Volkswagen Grand California\n2-Volkswagen Grand California Camper Full Equip"+ RESET)
     val cotxeUsuari=llegirInt(1, 2,messageError)
 
-    println("Introdueixi els kilometres del vehicle (Ex: 0,00)")
+    println(YELLOW_BOLD+"Introdueixi els kilometres del vehicle (Ex: 0,00)"+RESET)
     val kilometresCotxe=llegirDouble(0, messageError)
 
     //pregunta l'estat dels pneumatics
-    println("Introdueixi quants kilometres tenen els pneumatics")
+    println(GREEN_BOLD+"Introdueixi quants kilometres tenen els pneumatics"+RESET)
     val estatPneumatics=llegirLong(0,messageError)
 
     //pregunta a l'usuari si la furgo disposa o ni de portabicis
-    println("Sel·leccioni una opció:\n1-Disposa de portabicis\n2-No disposa de portabicis")
+    println(PURPLE_BOLD+"Sel·leccioni una opció:\n1-Disposa de portabicis\n2-No disposa de portabicis"+RESET)
     val portabicisUsuari= llegirInt(1,2,messageError)
 
     //pregunta a l'usuari els anys de la furgo
-    println("Introdueixi els anys d'antiguitat de la furgoneta")
+    println(BLUE_BOLD+"Introdueixi els anys d'antiguitat de la furgoneta"+RESET)
     val anysFurgo= llegirInt(1,50,messageError)
 
     //calcula el preu inicial segons model
@@ -34,5 +35,5 @@ fun main(){
     val preuBici=calculPortabicis(portabicisUsuari,preuFinal)
     val preuAnys=calculPerduaAnys(anysFurgo,preuFinal,kilometresCotxe)
 
-    print("El preu final es: "+preuFinal+"€")
+    print(RED_BACKGROUND+"El preu final es: "+preuFinal+"€"+ RESET)
 }
